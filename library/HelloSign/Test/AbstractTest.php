583b43b1af86dc9dc288e6d3755fd7f720bd8696
Chris Paul <chris@hellosign.com>
2014-10-13 16:36:05 -0700

    Fix API test.

--- library/HelloSign/Test/AbstractTest.php
+++ library/HelloSign/Test/AbstractTest.php
@@ -25,7 +25,7 @@ abstract class AbstractTest extends \PHPUnit_Framework_TestCase
 
         $api_url = $_ENV['API_URL'] == null ? Client::API_URL : $_ENV['API_URL'];
         $oauth_token_url = $_ENV['OAUTH_TOKEN_URL'] == null ? Client::OAUTH_TOKEN_URL : $_ENV['OAUTH_TOKEN_URL'];
-        $this->client = new Client($_ENV['API_KEY'], null, $api_url, $api_token_url, $oauth_token_url);
+        $this->client = new Client($_ENV['API_KEY'], null, $api_url, $oauth_token_url);
         $this->team_member_1 = rand(1,10000000) . "@example.com";
         $this->team_member_2 = rand(1,10000000) . "@example.com";
      	//$this->client->enableDebugMode();
