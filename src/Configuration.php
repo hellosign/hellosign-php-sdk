e3c77e878ae4033e460c7b0ebea46a7cb53432ea
Juan Treminio <juan.treminio@dropbox.com>
2022-05-05 13:04:49 -0500

    Adding version to composer

--- src/Configuration.php
+++ src/Configuration.php
@@ -91,7 +91,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'OpenAPI-Generator/1.0.0/PHP';
+    protected $userAgent = 'OpenAPI-Generator/v6.0.0-beta22/PHP';
 
     /**
      * Debug switch (default set to false)
@@ -457,6 +457,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 3.0.0' . PHP_EOL;
+        $report .= '    SDK Package Version: v6.0.0-beta22' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
