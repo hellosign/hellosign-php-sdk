9eac26f85a4ad4785b013d0b57d120a79f2e4553
Juan Treminio <50673996+jtreminio-dropbox@users.noreply.github.com>
2022-06-24 11:04:56 -0500

    Rename "checked" parameter to "is_checked" (#144)

--- src/Configuration.php
+++ src/Configuration.php
@@ -91,7 +91,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'OpenAPI-Generator/v6.0.0-beta24/PHP';
+    protected $userAgent = 'OpenAPI-Generator/v6.0.0-beta25/PHP';
 
     /**
      * Debug switch (default set to false)
@@ -457,7 +457,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 3.0.0' . PHP_EOL;
-        $report .= '    SDK Package Version: v6.0.0-beta24' . PHP_EOL;
+        $report .= '    SDK Package Version: v6.0.0-beta25' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
